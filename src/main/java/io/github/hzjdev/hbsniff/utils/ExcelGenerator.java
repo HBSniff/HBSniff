279adddfe1090f382cdda41654b6953c2134e474
escapar <leyline@qq.com>
2021-09-11 13:34:48 +0800

    change project name

--- src/main/java/io/github/hzjdev/hbsniff/utils/ExcelGenerator.java
+++ src/main/java/io/github/hzjdev/hbsniff/utils/ExcelGenerator.java
@@ -15,30 +15,7 @@
  *     along with HBSniff.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package tests.io.github.hzjdev.hqlsniffer.entities;
+package io.github.hzjdev.hbsniff.utils;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import java.util.Collection;
-
-@Entity
-@Table(name = "Dummy")
-public class OneToManyLazyBatchSizeEntity {
-
-    @BatchSize(size = 4)
-    @OneToMany(fetch = FetchType.LAZY)
-    public Collection<ManyToOneEntity> lst;
-    @Id
-    private String id;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
+public class ExcelGenerator {
 }
